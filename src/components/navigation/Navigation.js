d76c3b66e219053e4b0a1df2034dfe576e1202ec
josh <jjhouston24@gmail.com>
2019-06-18 10:47:45 -0500

    added some styling

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import dashboard from './dashboard.png';
+import heart from './heart.png';
+import logout from './logout.png';
+import add from './add.png';
+import home from './home.png';
 
 
 
 function Navigation(props) {
     return (
         <nav className='nav-main'>
-            <ul>
-                <li>Home</li>
-                <button onClick={props.logOut} >Logout</button>
+            <ul className='nav-col'>
+            <div className='nav-row'>
+                        <img src={home} alt=""/>
+                </div>
+
+                <div className='nav-row'>
+                        <img src={dashboard} alt=""/>
+                </div>
+
+                <div className="nav-row">
+                        <img src={heart} alt=""/>
+                 </div>
+
+                <div className="nav-row">
+                        <img src={add} alt=""/>
+                </div>
             </ul>
+                <button onClick={props.logOut} ><img src={logout} className='logout' alt=""/></button>
         </nav>
     )    
 }
